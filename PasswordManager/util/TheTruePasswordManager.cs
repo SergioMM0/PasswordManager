2394a13ce38ec245b48f10e54f611ec33d6466cd
Sergio M <sergiomorenomartinez544@gmail.com>
2024-10-11 22:18:55 +0200

    format

--- PasswordManager/util/TheTruePasswordManager.cs
+++ PasswordManager/util/TheTruePasswordManager.cs
@@ -27,17 +27,17 @@ public static class TheTruePasswordManager {
         var enteredHashedPassword = Convert.ToBase64String(hashBytes);
         return enteredHashedPassword == storedHashedPassword;
     }
-    
+
     public static string GenerateSecurePassword(int length) {
         const string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-        char[] password = new char[length];
-        byte[] randomBytes = new byte[length];
+        var password = new char[length];
+        var randomBytes = new byte[length];
 
         using (var rng = RandomNumberGenerator.Create()) {
             rng.GetBytes(randomBytes);
         }
 
-        for (int i = 0; i < length; i++) {
+        for (var i = 0; i < length; i++) {
             password[i] = validChars[randomBytes[i] % validChars.Length];
         }
 
